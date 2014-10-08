20d8f771ea53f8bc9499ae105f35b9d30a4ab40b
Hyunsang Sun <asunhs@gmail.com>
2014-10-08 10:47:00 +0900

    pre calculate point

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -13,46 +13,6 @@
     
     angular.module('YahtzeeApp').controller('MainCtrl', function ($scope, $q, $timeout, $interval, DiceSvc) {
         
-    
-        function RollingGame (option) {
-
-            if (!(this instanceof RollingGame)) {
-                return new RollingGame(option);
-            }
-
-            var times = 0,
-                game = this,
-                eyes = [],
-                n = option.n || 3,
-                roll = option.roll || _.identity,
-                done = option.done || _.identity;
-
-            game.canRolling = function () {
-                return times < n;
-            };
-
-            game.roll = function () {
-                if (!game.canRolling()) {
-                    throw new Error('not valid');
-                }
-
-                times++;
-                var rolled = $q.defer();
-
-                roll(rolled);
-
-                rolled.promise.then(function (results) {
-                    eyes = results;
-                });
-            };
-
-            game.done = function (slot) {
-                times = 0;
-                done(eyes, slot);
-            };
-
-        }
-        
         var total = function (eyes) { return _.reduce(eyes, function (sum, eye) { return sum + eye; }, 0); },
             upper = function (n, eyes) { return _.chain(eyes).filter(function (eye) { return eye == n; }).reduce(function (sum, eye) { return sum + eye; }, 0).value(); },
             nkind = function (n, eyes) { return _.chain(eyes).countBy(function (eye) { return eye; }).find(function (count) { return count >= n; }).value(); },
@@ -65,61 +25,31 @@
         
         
         var rules = [
-                _.partial(upper, 1),
-                _.partial(upper, 2),
-                _.partial(upper, 3),
-                _.partial(upper, 4),
-                _.partial(upper, 5),
-                _.partial(upper, 6),
-                _.partial(akind, 3),
-                _.partial(akind, 4),
-                function (eyes) {
-                    var counts = _.chain(eyes).countBy(function (eye) { return eye; });
-                    
-                    if (counts.find(function (count) { return count = 2; }).value()
-                        && counts.find(function (count) { return count = 3; }).value()) {
-                        return 25;
-                    }
-                    
-                    return 0;
-                },
-                _.partial(contain, [[1,2,3,4], [2,3,4,5], [3,4,5,6]], 30),
-                _.partial(contain, [[1,2,3,4,5], [2,3,4,5,6]], 40),
-                function (eyes) { return nkind(5, eyes) ? 50 : 0; },
-                total
-            ],
-            game = new RollingGame({
-            roll : function (rolled) {
-                _.each(dices, function (dice) { return dice.rolling(); });
-            
-                var deferred = $q.defer();
+            _.partial(upper, 1),
+            _.partial(upper, 2),
+            _.partial(upper, 3),
+            _.partial(upper, 4),
+            _.partial(upper, 5),
+            _.partial(upper, 6),
+            _.partial(akind, 3),
+            _.partial(akind, 4),
+            function (eyes) {
+                var counts = _.chain(eyes).countBy(function (eye) { return eye; });
 
-                DiceSvc.rollingDice(rolled);
-                $timeout(deferred.resolve, 1000);
+                if (counts.find(function (count) { return count = 2; }).value()
+                    && counts.find(function (count) { return count = 3; }).value()) {
+                    return 25;
+                }
 
-                $q.all([rolled.promise, deferred.promise]).then(function (results) {
-
-                    var eyes = results[0];
-
-                    _.each(eyes, function (eye) {
-                        return $timeout(function () {
-                            dices[eye.seq].stop(eye.eye);
-                        }, Math.floor(Math.random() * 3) * 300);
-                    });
-                });
+                return 0;
             },
-            done : function (eyes, slot) {
-                var result = _
-                    .chain(eyes)
-                    .sortBy(function (eye) { return eye.seq; })
-                    .map(function (eye) { return eye.eye + 1; })
-                    .value();
-                
-                console.log(rules[slot](result));
-                
-                DiceSvc.decisionSlot(slot, eyes);
-            }
-        });
+            _.partial(contain, [[1,2,3,4], [2,3,4,5], [3,4,5,6]], 30),
+            _.partial(contain, [[1,2,3,4,5], [2,3,4,5,6]], 40),
+            function (eyes) { return nkind(5, eyes) ? 50 : 0; },
+            total
+        ];
+        
+        var eyes = null;
         
         $scope.isFilled = function (slot) {
             return models.me.game[slot] !== null;
@@ -129,13 +59,42 @@
             return models.me.game && { done : (models.me.game[slot] != null), slot : (models.me.game[slot] == null) };
         };
         
-        $scope.roll = game.roll;
+        $scope.roll = function () {
+            _.each(dices, function (dice) { return dice.rolling(); });
+
+            var deferred = $q.defer(),
+                rolled = $q.defer();
+
+            DiceSvc.rollingDice(rolled);
+            $timeout(deferred.resolve, 1000);
+
+            $q.all([rolled.promise, deferred.promise]).then(function (results) {
+
+                var rolledEyes = results[0];
+
+                _.each(rolledEyes, function (eye) {
+                    return $timeout(function () {
+                        dices[eye.seq].stop(eye.eye);
+                    }, Math.floor(Math.random() * 3) * 150);
+                });
+                
+                eyes = _.chain(rolledEyes).sortBy(function (eye) { return eye.seq; }).map(function (eye) { return eye.eye + 1; }).value();
+            });
+        };
         
-        $scope.done = game.done;
+        $scope.done = function (slot) {
+            console.log(rules[slot](eyes));
+            DiceSvc.decisionSlot(slot, eyes);
+            eyes = null;
+        };
         
         $scope.models = models;
         
-        $scope.rules = [
+        $scope.getPoint = function (slot) {
+            return !models.me.game ? null : models.me.game[slot] !== null ? models.me.game[slot] : eyes !== null ? rules[slot](eyes) : null;
+        };
+        
+        $scope.explanations = [
             'Count and Add Only Aces',
             'Count and Add Only Twos',
             'Count and Add Only Threes',
