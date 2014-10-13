4a200054e495b9e411e13ded4b2663f26cffb310
chris <ikaruce@gmail.com>
2014-10-13 23:15:17 +0900

    finish_frame

---
+++ app/controllers/yahtzee_controller.rb
@@ -0,0 +1,18 @@
+class YahtzeeController < ApplicationController
+  layout false
+
+  def role
+  end
+
+  def enroll
+  end
+
+  def user
+  end
+
+  def index
+  end
+
+  def players
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'role' => 'yahtzee#role'
+  get 'enroll' => 'yahtzee#enroll'
+  get 'user' => 'yahtzee#user'
+  get 'players' => 'yahtzee#players'
+
+  root 'yahtzee#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
